96eb45770261dfc686aa4b85f2db2914181e5728
parkergelinas <87443891+parkergelinas@users.noreply.github.com>
2022-07-01 01:01:54 -0700

    updates

---
+++ Practice/arrays/AccessElements.js
@@ -0,0 +1,16 @@
+const famousSayings = [
+  "Fortune favors the brave.",
+  "A joke is a very serious thing.",
+  "Where there is love there is life.",
+];
+
+let listItem = famousSayings[0];
+
+// First item
+console.log(listItem);
+
+// Third item
+console.log(famousSayings[2]);
+
+// Undefined item
+console.log(famousSayings[3]);
---
+++ Practice/arrays/LETvsCONST.js
@@ -0,0 +1,13 @@
+/// Let Array
+let condiments = ["Ketchup", "Mustard", "Soy Sauce", "Sriracha"];
+
+condiments[0] = "Mayo";
+console.log(condiments);
+
+condiments = ["Mayo"];
+
+/// Const Array
+const utensils = ["Fork", "Knife", "Chopsticks", "Spork"];
+
+utensils[3] = "Spoon";
+console.log(utensils);
